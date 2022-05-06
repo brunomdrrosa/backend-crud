93d181db32d3a3608d6fb0eb38bc360c4920518a
Matheus Gunnar Marques da Silva <matheus.silva@dbserver.com.br>
2022-05-06 14:38:45 -0300

    Configurações de DB POSTGRES

--- src/main/java/com/crud/properties/PropertiesApplication.java
+++ src/main/java/com/crud/properties/PropertiesApplication.java
@@ -2,12 +2,15 @@ package com.crud.properties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class PropertiesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PropertiesApplication.class, args);
+
 	}
 
 }
