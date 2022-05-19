dcacdca23e6c3f8499473f4b355f2fbc4ec9d7f6
Matheus Gunnar <matheus.silva@spmassets.com>
2022-05-19 10:59:39 -0300

    Refactoring

--- src/main/java/com/crud/properties/controllers/PropertyController.java
+++ src/main/java/com/crud/properties/controllers/PropertyController.java
@@ -9,40 +9,51 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/properties")
 @Tag(name = "Property", description = "Property Controller")
 public class PropertyController {
-    
-    PropertyRepository PropertyRepository;
-    @Autowired
-    private PropertyServices service;
-
-    @GetMapping
-    @Operation(summary = "Get all the properties", description = "Get a list of all the %properties% registered in the system.")
-    @ApiResponse(responseCode = "200",description = "Successful Operation.")
-    public List<Property> getProperties() {
-        return service.findAll();
-    }
-    
-    @PutMapping("/{id}")
-    @Operation(summary = "Update a property", description = "Update a property.")
-    public Property updateProperty(@PathVariable Long id, @RequestBody Property newProperty) {
-    return service.updateProperty(newProperty);
-    }
-    
-    @GetMapping("/{id}")
-    @Operation(summary = "Get a property by ID", description = "Get a property by ID.")
-    public Property getPropertyById(@PathVariable Long id) {
-        return service.findById(id);
-    }
-
-    @DeleteMapping("/{id}")
-    @Operation(summary = "Delete a property by ID", description = "Delete a property by ID.")
-    public ResponseEntity<String> deletePropertyById(@PathVariable Long id) {
-        service.deleteProperty(id);
-        return ResponseEntity.ok("Property deleted");
-    }
+	
+	PropertyRepository PropertyRepository;
+	@Autowired
+	private PropertyServices service;
+	
+	@GetMapping("/{id}")
+	@Operation(summary = "Get a property by ID", description = "Get a property by ID.")
+	public Property getPropertyById(@PathVariable Long id) {
+		return service.findById(id);
+	}
+	
+	@GetMapping("/")
+	@Operation(summary = "Get all the properties", description = "Get a list of all the %properties% registered in the system.")
+	@ApiResponse(responseCode = "200", description = "Successful Operation.")
+	public List<Property> getProperties() {
+		return service.findAll();
+	}
+	
+	@PostMapping("/")
+	@Operation(summary = "Create a property", description = "Create a property.")
+	@ApiResponse(responseCode = "201", description = "Property created.")
+	public Property createProperty(@RequestBody Property property) {
+		return service.addProperty(property);
+	}
+	
+	
+	@PutMapping("/{id}")
+	@Operation(summary = "Update a property", description = "Update a property.")
+	@ApiResponse(responseCode = "200", description = "Property updated.")
+	public Property updateProperty(@PathVariable Long id, @RequestBody Property newProperty) {
+		return service.updateProperty(newProperty);
+	}
+	
+	@DeleteMapping("/{id}")
+	@Operation(summary = "Delete a property by ID", description = "Delete a property by ID.")
+	@ApiResponse(responseCode = "204", description = "Property deleted.")
+	public ResponseEntity<String> deletePropertyById(@PathVariable Long id) {
+		service.deleteProperty(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/crud/properties/services/PropertyServices.java
+++ src/main/java/com/crud/properties/services/PropertyServices.java
@@ -5,8 +5,12 @@ import com.crud.properties.repositories.PropertyRepository;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
 
 @Service
@@ -20,22 +24,37 @@ public class PropertyServices {
     }
     
     public Property addProperty(Property newProperty){
-       return repository.save(newProperty);
+     
+        if (repository.existsById(newProperty.getId())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Property already exists for this id");
+        } else {
+            return repository.save(newProperty);
+        }
     }
     
     public Property updateProperty(@NotNull Property updatedProperty){
        if (repository.existsById(updatedProperty.getId())){
        return repository.save(updatedProperty);
     } else {
-        return null;
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Property not found for this id");
        }
    }
     
-    public void deleteProperty(Long id){
-        repository.deleteById(id);
+    public ResponseEntity deleteProperty(Long id){
+        if (repository.existsById(id)){
+            repository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Property deleted");
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Property not found for this id");
     }
     
     public Property findById(Long id){
+        if (repository.existsById(id)){
         return repository.findById(id).get();
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Property not found for this id");
+    
     }
+    
+    
 }
