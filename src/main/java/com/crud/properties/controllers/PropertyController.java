a5f8a4865d6cc78f206587902809397711544849
Matheus Gunnar <matheus.silva@spmassets.com>
2022-05-18 22:27:39 -0300

    Delete e update funcionando

--- src/main/java/com/crud/properties/controllers/PropertyController.java
+++ src/main/java/com/crud/properties/controllers/PropertyController.java
@@ -1,20 +1,15 @@
 package com.crud.properties.controllers;
 
-
 import com.crud.properties.entities.Property;
 import com.crud.properties.repositories.PropertyRepository;
 import com.crud.properties.services.PropertyServices;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/properties")
@@ -26,66 +21,28 @@ public class PropertyController {
     private PropertyServices service;
 
     @GetMapping
-
     @Operation(summary = "Get all the properties", description = "Get a list of all the %properties% registered in the system.")
     @ApiResponse(responseCode = "200",description = "Successful Operation.")
     public List<Property> getProperties() {
         return service.findAll();
-
     }
+    
     @PutMapping("/{id}")
-    @Operation(summary = "Update a new property", description = "Update a new property in the database.")
+    @Operation(summary = "Update a property", description = "Update a property.")
     public Property updateProperty(@PathVariable Long id, @RequestBody Property newProperty) {
-        
-        return service.updateProperty(newProperty);
-        
+    return service.updateProperty(newProperty);
     }
     
+    @GetMapping("/{id}")
+    @Operation(summary = "Get a property by ID", description = "Get a property by ID.")
+    public Property getPropertyById(@PathVariable Long id) {
+        return service.findById(id);
+    }
 
-    @PostMapping
-    @Operation(summary = "Create a new property", description = "Insert a new property in the database.")
-    @Parameter(required = true)
-    public Property addProperty(@RequestBody Property newProperty) {
-        List<Property> listProperties = service.findAll();
-        for (Property property : listProperties) {
-            System.out.println(listProperties);
-            if (property.getId().equals(property.getId())) {
-                return service.updateProperty(newProperty);
-            }else {
-                System.out.println("Property does not exists");
-            }
-        }
-//        try {
-//
-//        } catch
-//        (Exception e) {}
-//
-        //return service.addProperty(newProperty);
-
-    
-    
-  
-    
-    
-    
-    
-   
-  
-   
-//    @PutMapping("/{id}")
-//    @Transactional
-//    public ResponseEntity<PropertyDTO> update(@PathVariable Long id,
-//                                              @RequestBody @Valid UpdatePropertyForm form) {
-//        Optional<Property> optional = PropertyRepository.findById(id);
-//        if (optional.isPresent()) {
-//
-//            Property property1 = form.update(id, PropertyRepository);
-//            return ResponseEntity.ok(new PropertyDTO(property1));
-//        }
-//
-//        return ResponseEntity.notFound().build();
-//    }
-    
-        return newProperty;
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a property by ID", description = "Delete a property by ID.")
+    public ResponseEntity<String> deletePropertyById(@PathVariable Long id) {
+        service.deleteProperty(id);
+        return ResponseEntity.ok("Property deleted");
     }
 }
--- src/main/java/com/crud/properties/services/PropertyServices.java
+++ src/main/java/com/crud/properties/services/PropertyServices.java
@@ -3,16 +3,14 @@ package com.crud.properties.services;
 import com.crud.properties.entities.Property;
 import com.crud.properties.repositories.PropertyRepository;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
-
 
 @Service
 public class PropertyServices {
-
     @Autowired
     private PropertyRepository repository;
    @Transactional
@@ -21,15 +19,23 @@ public class PropertyServices {
        return (List<Property>) repository.findAll();
     }
     
-    
-
     public Property addProperty(Property newProperty){
        return repository.save(newProperty);
     }
     
-    public Property updateProperty(Property updatedProperty){
-        return repository.save(updatedProperty);
+    public Property updateProperty(@NotNull Property updatedProperty){
+       if (repository.existsById(updatedProperty.getId())){
+       return repository.save(updatedProperty);
+    } else {
+        return null;
+       }
+   }
+    
+    public void deleteProperty(Long id){
+        repository.deleteById(id);
     }
-
-
+    
+    public Property findById(Long id){
+        return repository.findById(id).get();
+    }
 }
